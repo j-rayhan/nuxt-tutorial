5335a39e538a5f73331c416f67b84f5b11352c45
johir-rayhan <johir.rayhan01@gmail.com>
2020-08-02 17:58:02 +0600

    Creating Nested Routes

---
+++ pages/users.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <h1>This is users index page</h1>
+    <hr />
+    <input v-model="userId" type="text" />
+    <button @click="onLoadUser">Load user</button>
+    <nuxt-child />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: '',
+    }
+  },
+  methods: {
+    onLoadUser() {
+      console.log('---->', this.userId)
+      this.$router.push('/users/' + this.userId)
+    },
+  },
+}
+</script>
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,25 +1,3 @@
 <template>
-  <div class="container">
-    <h1>This is users index page</h1>
-    <hr />
-    <input v-model="userId" type="text" />
-    <button @click="onLoadUser">Load user</button>
-  </div>
+  <h1>Input user id then click Load button</h1>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      userId: '',
-    }
-  },
-  methods: {
-    onLoadUser() {
-      console.log('---->', this.userId)
-      this.$router.push('/users/' + this.userId)
-    },
-  },
-}
-</script>
-<style></style>
