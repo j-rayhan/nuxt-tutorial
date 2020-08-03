1cd6f2d04ea5689bf89d1b0ea2ec5c8c58f54232
johir-rayhan <johir.rayhan01@gmail.com>
2020-08-03 17:29:15 +0600

    Adding the Header Component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,24 @@
 <template>
   <div>
+    <Header @sidenavToggle="displaySidenav = !displaySidenav" />
+    <Sidenav :show="displaySidenav" @close="displaySidenav = false" />
     <Nuxt />
   </div>
 </template>
 
 <script>
-export default {}
+import Header from '@/components/Navigation/Header'
+import Sidenav from '@/components/Navigation/Sidenav'
+export default {
+  components: {
+    Header,
+    Sidenav,
+  },
+  data() {
+    return {
+      displaySidenav: false,
+    }
+  },
+}
 </script>
 <style></style>
